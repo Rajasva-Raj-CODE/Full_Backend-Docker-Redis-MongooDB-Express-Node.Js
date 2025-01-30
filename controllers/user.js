b5cbeb5fc3047a92078102af9daede4ae4384e6d
Rajasva-Raj-CODE <rajasvarajshrivastava@gmail.com>
2025-01-30 20:55:13 +0530

    user created

---
+++ controllers/user.js
@@ -0,0 +1,21 @@
+import { User } from "../models/user";
+
+export const register = async (req, res) => {
+  try {
+    const { fullName, email, password } = req.body;
+    if (!fullName || !email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Please fill all the fields", success: false });
+    }
+    // finding user ki with this email id se register toh ni
+    const user = await User.findOne({ email });
+    if (user) {
+      return res
+        .status(403)
+        .json({ success: false, message: "This email id is already registered" });
+    }
+    await User.create({ fullName, email, password });
+    return res.status(201).json({ success: true, message: "User Registered" }); 
+  } catch (error) {}
+};
