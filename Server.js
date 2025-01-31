67b2086bbc6252956872137f09fa4aa607b66b99
Rajasva-Raj-CODE <rajasvarajshrivastava@gmail.com>
2025-01-31 22:27:28 +0530

    login done

--- Server.js
+++ Server.js
@@ -2,12 +2,19 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./DB/database.js";
 import userRoutes from "./routes/user.js";
+import bodyParser from "body-parser";
 
 const app = express();
 dotenv.config();
 
 connectDB();
 
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+//http://localhost:8000/api/v1/user/
 app.use("/api/v1/user",userRoutes);
 
 
--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 import { User } from "../models/user.js";
 import bcrypt from "bcrypt";
+
 export const register = async (req, res) => {
   try {
     const { fullName, email, password } = req.body;
@@ -11,14 +12,45 @@ export const register = async (req, res) => {
     // finding user ki with this email id se register toh ni
     const user = await User.findOne({ email });
     if (user) {
-      return res
-        .status(403)
-        .json({ success: false, message: "This email id is already registered" });
+      return res.status(403).json({
+        success: false,
+        message: "This email id is already registered",
+      });
     }
     // password hashing
-    const hashedPassword = await bcrypt.hash(password,10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     await User.create({ fullName, email, password: hashedPassword });
-    return res.status(201).json({ success: true, message: "User Registered" }); 
-  } catch (error) {}
+    return res.status(201).json({ success: true, message: "User Registered" });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Please fill all the fields" });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Incorrect email id or password" });
+    }
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Incorrect email id or password" });
+    }
+    return res
+      .status(200)
+      .json({ success: true, message: `Welcome ${user.fullName}` });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
 };
