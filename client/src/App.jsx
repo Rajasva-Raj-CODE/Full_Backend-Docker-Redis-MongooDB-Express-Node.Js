b08099023265aa921cde54c44757fa0f23b21f31
Rajasva-Raj-CODE <rajasvarajshrivastava@gmail.com>
2025-02-04 22:01:25 +0530

    clieent side started

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './App.css'
+import Navbar from '@/pages/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Vite!</h1>
+    <div>
+      <Navbar/>
     </div>
   )
 }
---
+++ client/src/pages/Navbar.jsx
@@ -0,0 +1,16 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-gray-600'>
+      <div>
+        
+      </div>
+    <h1>{"Rajasva Raj Srivastava"}</h1>
+        <Button>Logout</Button>
+    </div>
+  )
+}
+
+export default Navbar
